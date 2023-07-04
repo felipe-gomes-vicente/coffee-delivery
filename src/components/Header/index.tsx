4d5d2ee4c36e9edddaa1415d53fba83d685ea00a
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-07-04 11:18:07 -0300

    created component Header and styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { MapPin, ShoppingCart } from "phosphor-react";
+
+import coffeeLogoImg from '../../assets/coffee-delivery-logo.svg'
+
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <div className='container' >
+        <img src={coffeeLogoImg} alt="" />
+
+        <HeaderButtonsContainer>
+          <HeaderButton variant="purple">
+            <MapPin size={20} weight="fill" />
+            São Paulo, SP
+          </HeaderButton>
+          <HeaderButton variant="yellow">
+            <ShoppingCart size={20} weight="fill" />
+          </HeaderButton>
+        </HeaderButtonsContainer>
+      </div>
+    </HeaderContainer>
+  )
+}

487d5bb0322ade4fbb4baa88b8e73e2bb18b5112
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-07-24 08:25:44 -0300

    install immer,, created useCart and  created function addCoffeeToCart

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
 import { Router } from "./Routes"
+import { CartContextProvider } from "./contexts/CartContext"
 
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
-import { CartContextProvider } from "./contexts/CartContext"
 
 function App() {
 

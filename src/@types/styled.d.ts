5cb6045ebc9f3272f780c22da4a02621df202cfc
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-08-08 08:07:54 -0300

    fix eslint DefaultTheme
    eslint-disable-next-line @typescript-eslint/no-empty-interface

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,6 +4,6 @@ import { defaultTheme } from '../styles/themes/default'
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  export type DefaultTheme = ThemeType
-  // previous version export interface DefaultTheme extends ThemeType {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends ThemeType {}
 }
